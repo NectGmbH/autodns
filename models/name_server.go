8c783ffac8f52e60712b0ac760d18f916f24712f
Patrick Kavajin <pk@nect.com>
2019-07-22 17:03:19 +0200

    REVERT: make ns ip address optional

--- models/name_server.go
+++ models/name_server.go
@@ -18,7 +18,7 @@ import (
 type NameServer struct {
 
 	// The ip addresses.
-	IPAddresses []string `json:"ipAddresses,omitempty"`
+	IPAddresses []string `json:"ipAddresses"`
 
 	// Host name of the nameserver.
 	// Required: true
